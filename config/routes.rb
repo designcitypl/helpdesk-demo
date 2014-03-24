b52e0181089e79dfa31a3ab7aae9dfee440ea08c
Wacław Łuczak <waclaw@luczak.it>
2014-03-24 21:10:04 +0100

    added roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 DemoHelpdesk::Application.routes.draw do
   devise_for :users
+
+  root :to => 'home#index'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
+
+puts 'ROLES'
+YAML.load(ENV['ROLES']).each do |role|
+  Role.find_or_create_by_name(role)
+  puts 'role: ' << role
+end
+
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+admin = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+admin.add_role :admin
+puts 'admin: ' << admin.name
+user = User.find_or_create_by_email :name => ENV['USER_NAME'].dup, :email => ENV['USER_EMAIL'].dup, :password => ENV['USER_PASSWORD'].dup, :password_confirmation => ENV['USER_PASSWORD'].dup
 puts 'user: ' << user.name
